00774425d9d08c9b734cd18262ca8a107d12795c
Jayraj Rathod <jayrahod5@gmail.com>
2022-05-18 18:00:26 +0530

    Styling search bar

--- components/Search.jsx
+++ components/Search.jsx
@@ -3,6 +3,35 @@ import { getStory } from '../functions/story';
 import styles from '../styles/Layout.module.css';
 import AsyncSelect from 'react-select/async';
 
+const customStyles = {
+	control: (styles, { isFocused }) => ({
+		...styles,
+		backgroundColor: '#fff',
+		fontSize: '1.1rem',
+		borderRadius: '8px',
+		border: isFocused ? '2px solid #ec7267' : '2px solid #c2c2c2',
+		outline: 'none',
+		width: '100%',
+		boxShadow: '2px 2px 5px #eee',
+		'&:hover ': {
+			border: '2px solid #ec7267',
+			cursor: 'pointer',
+		},
+	}),
+	option: (styles) => {
+		return {
+			...styles,
+			backgroundColor: '#fff',
+			color: 'black',
+			cursor: 'default',
+			'&:hover ': {
+				background: '#eee',
+				cursor: 'pointer',
+			},
+		};
+	},
+};
+
 const Search = ({}) => {
 	const [keyWord, setKeyWord] = useState('');
 	const [searchOpt, setSearchOpt] = useState([]);
@@ -29,11 +58,13 @@ const Search = ({}) => {
 	return (
 		<div>
 			<AsyncSelect
+				styles={customStyles}
 				classNamePrefix='select'
 				isSearchable={true}
 				isClearable={true}
 				cacheOptions
 				defaultOptions
+				placeHolder='Search by stories'
 				loadOptions={handleSearchResult}
 				onChange={(option, { action }) => {
 					switch (action) {
