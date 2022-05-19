fb70011735c3d3d7f2fd99a4cb0e44345671fbc1
Jayraj Rathod <jayrahod5@gmail.com>
2022-05-19 22:26:46 +0530

    Mobile friendly

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/Navbar.module.css';
 const Navbar = () => {
 	const pages = ['About', 'Popular stories', 'Contact', 'Feedback'];
 	return (
-		<div className={styles.container} xs={12}>
+		<div className={styles.container}>
 			<section className={styles.logo}>LOGO</section>
 			<section className={styles.links}>
 				{pages.map((page, index) => {
